cfadbd07b89eb0b09f8801360d80bbb419fd7aaa
marcoreyes-design <marco.reyes@itsvc.edu.mx>
2025-11-10 20:33:59 -0600

    Add files via upload

---
+++ EduTask/includes/navbar_docente.php
@@ -0,0 +1,25 @@
+<!-- Estilos Dashboard Docente -->
+<link rel="stylesheet" type="text/css" href="../assets/css/dashboard_docente.css">
+<!-- Fredy-->
+<!-- Bootstrap 5-->
+<link href="../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<!-- Bootstrap Icons-->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+<!-- Fuente Inter-->
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet">
+
+<!--Estilos CSS-->
+<link rel="stylesheet" href="../assets/css/Puntuacion.css">
+
+<!--Marco CSS-->
+<link href="../../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="../../assets/css/cssactividad.css">
+<script src="../../assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+<link rel="stylesheet" href="../../assets/css/crear_actividad_2.css">
+
+<!--victor CSS-->
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons|Material+Icons+Outlined" rel="stylesheet">
+
+<!-- Luz -->
+<link rel="stylesheet" href="../assets/css/index.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
