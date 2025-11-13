c0e62d3889ded65f6721c19a16eb052cfac8a8d4
marcoreyes-design <marco.reyes@itsvc.edu.mx>
2025-11-13 13:41:56 -0600

    Update navbar_docente.php

--- EduTask/includes/navbar_docente.php
+++ EduTask/includes/navbar_docente.php
@@ -3,8 +3,10 @@
 <!-- Fredy-->
 <!-- Bootstrap 5-->
 <link href="../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
 <!-- Bootstrap Icons-->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
+
 <!-- Fuente Inter-->
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet">
 
@@ -12,19 +14,16 @@
 <link rel="stylesheet" href="../assets/css/Puntuacion.css">
 
 <!--Marco CSS-->
-<link href="../../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet" href="../../assets/css/cssactividad.css">
-<script src="../../assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+<link rel="stylesheet" href="../../assets/css/actividad_1.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="../../assets/css/crear_actividad_2.css">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
 
 <!--victor CSS-->
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons|Material+Icons+Outlined" rel="stylesheet">
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <link rel="stylesheet" href="../Assets/bootstrap/css/bootstrap.min.css">
-<script src="../Assets/bootstrap/js/bootstrap.bundle.min.js"></script>
 <link rel="stylesheet" href="../../assets/css/DetalleActividad2.css">
 
 <!-- Luz -->
-<link rel="stylesheet" href="../assets/css/index.css">
-
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="assets/css/index.css">
